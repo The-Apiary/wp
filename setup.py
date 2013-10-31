d11eb8ba7e41de304940f0dbc3bed80e2944198d
Nicholas Vitovitch <nvitovitch@theapiary.co>
2013-10-30 23:10:24 -0400

    Changes to comply with distutils.  gitignore ignores logs, builds

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from distutils.core import setup
 setup(name='wp',
       version='0.1.0',
       description='Wallpaper Manager',
-      package_dir={'', 'lib'},
+      package_dir={'wp': 'lib'},
+      packages=['wp'],
       scripts=['scripts/wp']
      )
